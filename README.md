4ce2b053d637fb69cb5ce2b5ea4bfc11469fdeb7
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2020-10-22 23:25:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phpmvc
+ PHP native web app crud
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//base url constant
+define('BASE_URL', 'http://localhost/phpmvc/public');
+
+//db config
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'laravel8app');
+define('DB_USER', 'root');
+define('DB_PASS', '');
